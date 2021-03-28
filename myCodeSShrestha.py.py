6ce735de62afac6ffa597f8e114cd0ed9ea8b6a5
saurabhstha <76564460+saurabhstha@users.noreply.github.com>
2021-03-28 18:26:55 -0400

    Exercise 2.1

---
+++ myCodeSShrestha.py.py
@@ -0,0 +1,18 @@
+
+print("Hello World! I wonder why that is always the default coding text to start with")
+add_numbers = int(2+2)
+subtract_numbers = int(4-3)
+Multiply_numbers = int(3 * 2)
+divide_numbers = int(4/2)
+subject = "DSC 530 "
+theme = "is about data quality."
+course520 = subject + theme
+new = ["hustle", "life", 12, 3]
+spring = "break"
+new.append(spring)
+learn = ("new", "to", "jupyter", "notebook")
+
+
+
+
+
